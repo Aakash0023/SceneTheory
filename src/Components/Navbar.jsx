79f9f6cedb3c5d89b8f2009a0625aed9fe40ced2
Aakash0023 <itspersonal0623@gmail.com>
2026-06-07 17:04:25 +0530

    Refactored styling and improved homepage UI

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,22 @@
 const Navbar = () => {
   return (
-    <nav>
-      <h2>SceneTheory</h2>
+    <nav className="navbar">
+      <div className="logo">
+        <div className="logo-icon">ST</div>
+
+        <h2>
+          Scene<span>Theory</span>
+        </h2>
+      </div>
+
+      <ul className="nav-links">
+        <li>Discover</li>
+        <li>Quizzes</li>
+        <li>Community</li>
+        <li>Lists</li>
+      </ul>
+
+      <button className="signup-btn">Sign Up</button>
     </nav>
   );
 };
