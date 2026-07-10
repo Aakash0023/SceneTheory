4a65b9ba7caca0a76b0542fb24cf4a2504297409
Aakash0023 <itspersonal0623@gmail.com>
2026-07-10 15:22:21 +0530

    fixed the api url for the production

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster
+      position="top-right"
+      toastOptions={{
+        duration: 3000,
+
+        style: {
+          background: "#111",
+          color: "#fff",
+          border: "1px solid #c8912a",
+          borderRadius: "12px",
+        },
+
+        success: {
+          iconTheme: {
+            primary: "#c8912a",
+            secondary: "#111",
+          },
+        },
+
+        error: {
+          iconTheme: {
+            primary: "#ff4d4f",
+            secondary: "#111",
+          },
+        },
+      }}
+    />
+
     <App />
   </React.StrictMode>
 );
