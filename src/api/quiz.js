ec86433699b12a304566d0670e4cfd90dc270f5b
Aakash0023 <itspersonal0623@gmail.com>
2026-07-14 10:10:08 +0530

    fixed the api issue

--- src/api/quiz.js
+++ src/api/quiz.js
@@ -2,8 +2,6 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 console.log("API URL =", API_URL);
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const generateQuiz = async (title, overview) => {
   const response = await fetch(`${API_URL}/quiz/generate`, {
     method: "POST",
